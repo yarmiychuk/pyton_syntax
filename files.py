5af5a37a34c0df8aeebbab3c5dcf20f4d0dd4c26
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-09-30 00:44:50 +0300

    files example added

---
+++ files.py
@@ -0,0 +1,19 @@
+# a - дописать данные в файл
+# r - чтение
+# w - запись
+
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'a')
+data.writelines(colors) # Разделителя не будет: redgreenblue
+data.close()
+
+path = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
+
+# При таком подходе выывать close() не надо
+with open(path, 'w'):
+    data = open('file.txt', 'a')
+    data.writelines(colors)
